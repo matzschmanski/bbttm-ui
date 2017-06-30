685904b6d2330b52e11063fc5c233da2853e30e8
geig006 <matthias.geiger@bertelsmann.de>
2017-06-30 10:37:36 +0200

    code cleanup, removed comments

--- index.js
+++ index.js
@@ -2,33 +2,33 @@ const express = require('express');
 const app = express();
 var http = require('http').Server(app);
 var port = process.env.PORT || 8080;
-
 var io = require('socket.io')(http);
-
 var currentColor = "blue";
 
 app.use(express.static('res'));
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-      console.log('a user connected');
-      socket.on('disconnect', function(){
-              console.log('user disconnected');
-      });
-			socket.on('changeColor', function(){
-				currentColor =getRandomColor();
-			  io.emit('changeColorTo', currentColor);
-			 });
+io.on('connection', function(socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
 
-			 socket.on('getCurrentColor', function(){
- 			  io.emit('currentColor', currentColor);
- 			 });
+  socket.on('changeColor', function() {
+    currentColor = getRandomColor();
+    io.emit('changeColorTo', currentColor);
+  });
+
+  socket.on('getCurrentColor', function() {
+    io.emit('currentColor', currentColor);
+  });
 });
 
-function getRandomColor(){
-	var letters = '0123456789ABCDEF';
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
   var color = '#';
   for (var i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
@@ -36,9 +36,6 @@ function getRandomColor(){
   return color;
 }
 
-// http.listen(3000, function(){
-// 	  console.log('listening on *:3000');
-// });
 http.listen(port, function() {
-	console.log('Our app is running on http://localhost:' + port);
+  console.log('Our app is running on http://localhost:' + port);
 });
--- res/js/custom.js
+++ res/js/custom.js
@@ -1,24 +1,22 @@
-  $(function() {
-    $( document ).ready(function() {
-      socket.emit('getCurrentColor');
-    // $('#colored').css("background-color", color);
-    });
-    var socket = io();
-    $('#colored').click(function() {
-      console.log("Handler for $('#clicker').click() called.",$('#clicker'));
-      socket.emit('changeColor');
-    });
+$(function() {
 
-    socket.on('currentColor', function(color){
-      console.log('current color: ',color);
-      $('#colored').css("background-color", color);
-      $('.filled').css("background-image", 'none');
+  var socket = io();
 
-    });
-
-    socket.on('changeColorTo', function(color){
-      console.log('color: ',color);
-      $('#colored').css("background-color", color);
-
-        });
+  $(document).ready(function() {
+    socket.emit('getCurrentColor');
   });
+
+  $('#colored').click(function() {
+    socket.emit('changeColor');
+  });
+
+  socket.on('currentColor', function(color) {
+    $('#colored').css("background-color", color);
+    $('.filled').css("background-image", 'none');
+
+  });
+
+  socket.on('changeColorTo', function(color) {
+    $('#colored').css("background-color", color);
+  });
+});
